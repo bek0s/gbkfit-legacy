92ec5e754febb3ad1ee7cb578a7afe4fffa5ef91
bek0s <geobekiaris@gmail.com>
2015-09-12 12:23:38 +1000

    Initial commit.

--- build/src/gbc/gbc/CMakeFiles/target_gbc_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../../lib/libgbc_static_d.a"
-)
--- build/src/gbkfit/gbkfit_models_galaxy_2d_omp/CMakeFiles/target_gbkfit_models_galaxy_2d_omp_shared.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../../../lib/libgbkfit_models_galaxy_2d_omp_shared_d.pdb"
-  "../../../lib/libgbkfit_models_galaxy_2d_omp_shared_d.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/target_gbkfit_models_galaxy_2d_omp_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
