239b1e2c48fa2be40d032c43403115fa7c631990
bek0s <geobekiaris@gmail.com>
2015-09-17 03:00:42 +1000

    Initial commit.

--- build/src/gbkfit/gbkfit_fitters_mpfit/CMakeFiles/target_gbkfit_fitter_mpfit_object.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/target_gbkfit_fitter_mpfit_object.dir/src/fitter_mpfit.cpp.o"
-  "CMakeFiles/target_gbkfit_fitter_mpfit_object.dir/src/mpfit/mpfit.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/target_gbkfit_fitter_mpfit_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
